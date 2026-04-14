525af727fb3cca2a44be1da476c02fcc4847c564
ysy <2452715415@qq.com>
2026-04-14 19:35:23 +0800

    后端初始化 1.初始化项目 2.添加依赖和配置 HuTool和接口文档knife4j 3.提供基础代码 4.提供基础接口 健康检查接口

---
+++ src/main/java/com/ye/yeaicodemother/YeAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.ye.yeaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YeAiCodeMotherApplication.class, args);
+    }
+
+}
